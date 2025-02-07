290cd44eed983ffae720689f67cf61aa58673091
Ezefeola <ezefeola@gmail.com>
2025-02-07 14:51:57 -0300

    Fixes

--- src/main/java/com/javalibraryapi/JavaLibraryApi/Domain/Models/BaseModel.java
+++ src/main/java/com/javalibraryapi/JavaLibraryApi/Domain/Models/BaseModel.java
@@ -3,7 +3,9 @@ package com.javalibraryapi.JavaLibraryApi.Domain.Models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
